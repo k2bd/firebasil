09984c50756f4bf84691b7ac719c725a422fcedf
Kevin Duff <kevinkelduff@gmail.com>
2022-02-24 21:31:02 +0000

    Tested RTDB

--- tests/integration/test_rtdb.py
+++ tests/integration/test_rtdb.py
@@ -1,9 +1,39 @@
 import asyncio
+from datetime import timedelta
 
 import pytest
 
 from firebasil.exceptions import RtdbEventStreamException
-from firebasil.rtdb import EventType, Rtdb, RtdbEvent, RtdbNode
+from firebasil.rtdb import EventType, Rtdb, RtdbEvent, RtdbNode, SizeLimit
+
+
+def example_filter_data():
+    return {
+        "aaa": {
+            "height": 123,
+            "weight": 999,
+            "stats": {
+                "wis": 10,
+                "int": 11,
+            },
+        },
+        "bbb": {
+            "height": 234,
+            "weight": 888,
+            "stats": {
+                "wis": 12,
+                "int": 13,
+            },
+        },
+        "ccc": {
+            "height": 345,
+            "weight": 777,
+            "stats": {
+                "wis": 9,
+                "int": 10,
+            },
+        },
+    }
 
 
 @pytest.mark.asyncio
@@ -20,6 +50,229 @@ async def test_set_and_get_rtdb_root(rtdb_root: RtdbNode):
 
     got_subdata = await rtdb_root.child("a").get()
     assert got_subdata == "b", got_subdata
+
+
+@pytest.mark.asyncio
+async def test_order_by_key(rtdb_root: RtdbNode):
+    """
+    order_by_key, and limit_to_last
+    """
+    node = rtdb_root / "abcd"
+    await node.set(example_filter_data())
+
+    result = await node.order_by_key().limit_to_last(2).get()
+    assert result == {
+        "bbb": {
+            "height": 234,
+            "weight": 888,
+            "stats": {
+                "wis": 12,
+                "int": 13,
+            },
+        },
+        "ccc": {
+            "height": 345,
+            "weight": 777,
+            "stats": {
+                "wis": 9,
+                "int": 10,
+            },
+        },
+    }
+
+
+# See k2bd/firebasil#6
+@pytest.mark.xfail
+@pytest.mark.asyncio
+async def test_order_by_value(rtdb_root: RtdbNode):
+    """
+    order_by_value, and limit_to_first
+    """
+    node = rtdb_root / "scores"
+    await node.set(
+        {
+            "a": 11,
+            "b": 7,
+            "c": 10,
+            "d": 9,
+            "e": 8,
+            "f": 6,
+        }
+    )
+
+    result = await node.order_by_value().limit_to_first(2).get()
+    assert result == {
+        "b": 7,
+        "f": 6,
+    }
+
+
+# See k2bd/firebasil#6
+@pytest.mark.xfail
+@pytest.mark.asyncio
+async def test_order_by_child(rtdb_root: RtdbNode):
+    """
+    Order by some child
+    """
+    node = rtdb_root / "abcd"
+    await node.set(example_filter_data())
+
+    result = await node.order_by("stats/int").limit_to_first(1).get()
+    assert result == {
+        "ccc": {
+            "height": 345,
+            "weight": 777,
+            "stats": {
+                "wis": 9,
+                "int": 10,
+            },
+        },
+    }
+
+
+@pytest.mark.asyncio
+async def test_order_by_priority(rtdb_root: RtdbNode):
+    """
+    order_by_priority
+    """
+    node = rtdb_root / "priotest"
+    await node.set(
+        {
+            "a": {
+                "name": "AAA",
+                ".priority": 1.0,
+            },
+            "b": {
+                "name": "BBB",
+                ".priority": 1.5,
+            },
+            "c": {
+                "name": "CCC",
+                ".priority": 0.5,
+            },
+        }
+    )
+
+    result = await node.order_by_priority().limit_to_last(1).get()
+    assert result == {"b": {"name": "BBB"}}
+
+
+@pytest.mark.asyncio
+async def test_start_at_and_end_at(rtdb_root: RtdbNode):
+    """
+    Start and end at
+    """
+    node = rtdb_root / "startEnd"
+    await node.set(
+        {
+            "a": 12,
+            "b": 44,
+            "c": 13,
+            "d": 0,
+            "e": 50,
+        }
+    )
+
+    result = await node.order_by_key().start_at("b").end_at("d").get()
+    assert result == {
+        "b": 44,
+        "c": 13,
+        "d": 0,
+    }
+
+
+@pytest.mark.asyncio
+async def test_equal_to(rtdb_root: RtdbNode):
+    """
+    Search equal to
+    """
+    node = rtdb_root / "startEnd"
+    await node.set(
+        {
+            "a": 12,
+            "b": 44,
+            "c": 13,
+            "d": 0,
+            "e": 50,
+        }
+    )
+
+    result = await node.order_by_key().equal_to("d").get()
+    assert result == {"d": 0}
+
+
+@pytest.mark.asyncio
+async def test_shallow_get(rtdb_root: RtdbNode):
+    """
+    Shallow get
+    """
+    node = rtdb_root / "shallowTest"
+    await node.set(example_filter_data())
+
+    result = await node.shallow().get()
+    assert result == {
+        "aaa": True,
+        "bbb": True,
+        "ccc": True,
+    }
+
+
+@pytest.mark.asyncio
+async def test_order_by_priority_with_export_format(rtdb_root: RtdbNode):
+    """
+    order_by_priority with export format
+    """
+    node = rtdb_root / "priotest"
+    await node.set(
+        {
+            "a": {
+                "name": "AAA",
+                ".priority": 1.0,
+            },
+            "b": {
+                "name": "BBB",
+                ".priority": 1.5,
+            },
+            "c": {
+                "name": "CCC",
+                ".priority": 0.5,
+            },
+        }
+    )
+
+    result = await node.order_by_priority().limit_to_last(1).export_format().get()
+    assert result == {
+        "b": {
+            "name": "BBB",
+            ".priority": 1.5,
+        },
+    }
+
+
+@pytest.mark.parametrize(
+    "timeout", [timedelta(seconds=1), timedelta(minutes=3), timedelta(seconds=1.1)]
+)
+@pytest.mark.asyncio()
+async def test_timeouts(rtdb_root: RtdbNode, timeout: timedelta):
+    """
+    Timeouts don't fail
+    """
+    node = rtdb_root / "timeoutWriteLimit"
+    await node.set(example_filter_data())
+    result = await node.timeout(timeout).get()
+    assert result == example_filter_data()
+
+
+@pytest.mark.parametrize("write_limit", [e for e in SizeLimit])
+@pytest.mark.asyncio()
+async def test_write_limits(rtdb_root: RtdbNode, write_limit: SizeLimit):
+    """
+    Write limits don't fail
+    """
+    node = rtdb_root / "timeoutWriteLimit"
+    await node.write_size_limit(write_limit).set(example_filter_data())
+    result = await node.get()
+    assert result == example_filter_data()
 
 
 @pytest.mark.asyncio
